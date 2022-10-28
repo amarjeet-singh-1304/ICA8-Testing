84604c2eece8a47acfea716601e261e96d818a95
Amarjeet Singh <amarjeetsingh@Amarjeets-MacBook-Air.local>
2022-10-27 17:42:42 -0700

    Removing warnings

--- src/Urinals.java
+++ src/Urinals.java
@@ -1,20 +1,19 @@
 //Author - Amarjeet Singh
 
-public class urinals {
+public class Urinals {
 
     public static boolean goodString(String str)
     {
-        if(str.replaceAll("\\s", "").length() == 0 || str.replaceAll("\\s", "").length() >=21 )
+        if(str.replaceAll("\\s", "").length() == 0 ||
+                str.replaceAll("\\s", "").length() >=21 )
             return false;
         boolean check = true;
         for(int i = 0; i < str.length()-1; i++)
         {
-            if(str.charAt(i) == '0')
-                continue;
-            else
             if (str.charAt(i) == '1' && str.charAt(i+1) == str.charAt(i))
             {
                 check = false;
+                break;
             }
         }
 
--- test/UrinalsTest.java
+++ test/UrinalsTest.java
@@ -1,35 +1,33 @@
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class urinalsTest {
+class UrinalsTest {
 
     @Test
     @DisplayName("====== Amarjeet Singh == Test goodString Method(Empty String) =======")
     void checkEmptyString() {
-        boolean check = urinals.goodString(" ");
+        boolean check = Urinals.goodString(" ");
         assertFalse(check);
     }
     @Test
     @DisplayName("====== Amarjeet Singh == Test goodString Method(Good String) =======")
     void checkGoodString() {
-        boolean check1 = urinals.goodString("01010");
+        boolean check1 = Urinals.goodString("01010");
         assertTrue(check1);
     }
 
     @Test
     @DisplayName("====== Amarjeet Singh == Test goodString Method(Bad String) =======")
     void checkBadString() {
-        boolean check1 = urinals.goodString("01011");
+        boolean check1 = Urinals.goodString("01011");
         assertFalse(check1);
     }
 
     @Test
     @DisplayName("====== Amarjeet Singh == Test goodString Method(String Length >20) =======")
     void checkStringLength() {
-        boolean check1 = urinals.goodString("0101010101010101010101");
+        boolean check1 = Urinals.goodString("0101010101010101010101");
         assertFalse(check1);
     }
 
